2215b1c53656626f6cba43883f0f5e9dde7b552c
Mike Napier <mike.napier@advisorsexcel.com>
2017-11-02 20:36:48 -0500

    Initial Commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dfn\Bundle\OroCronofyBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
+
+/**
+ * @package Dfn\Bundle\OroCronofyBundle\DependencyInjection
+ * @author  Mike Napier <mike@napiercommunications.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('dfn_oro_cronofy');
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                'client_id' => ['value' => null],
+                'client_secret' => ['value' => null],
+                'access_token' => ['value' => null],
+            ]
+        );
+
+        return $treeBuilder;
+    }
+}
---
+++ DfnOroCronofyBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dfn\Bundle\OroCronofyBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DfnOroCronofyBundle
+ * @package Dfn\Bundle\OroCronofyBundle
+ */
+class DfnOroCronofyBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
